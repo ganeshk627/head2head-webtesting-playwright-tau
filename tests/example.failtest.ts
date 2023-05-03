6296b24929b9f14effc0f69154f13c0caf8b2c02
Ganesh K <ganeshkannan627@gmail.com>
2023-05-03 19:18:42 +0530

    modifying test failure

--- tests/example.failtest.ts
+++ tests/example.failtest.ts
@@ -3,4 +3,4 @@ import { test, expect } from '@playwright/test';
 test('simple failure test',async ({page}) => {
     await page.goto("https://my-json-server.typicode.com/ganeshk627/json-server-hogwarts")
     await expect(page).toHaveTitle(/My Json Server/)
-  })
+  });
